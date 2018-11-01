4aa2dcbf9ead712d4582a39bd2edc4c1d61b3574
Jan André Schlösser <jan.schloesser@it4need.de>
2018-11-01 18:31:25 +0100

    :bug: :checkered_flag: Windows support for paths

--- app/config/app.py
+++ app/config/app.py
@@ -3,12 +3,12 @@ import os
 
 
 class AppConfig(object):
-    try:
+    if os.name == 'nt':
+        root_dir = os.path.dirname(os.path.abspath(__file__)) + '\\..\\..\\'
+    else:
         root_dir = os.path.dirname(os.path.abspath(__file__)) + '/../../'
-    except:
-        root_dir = os.path.dirname(os.path.abspath(sys.executable)) + '/../../'
 
-    view_folder = root_dir + '/views'
+    view_folder = root_dir + 'views'
     view_extension = '.html'
-    database_folder = root_dir + '/data'
+    database_folder = root_dir + 'data'
     database_extension = '.json'
