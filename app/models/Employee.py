0ea1faa1668a7b2b389c4c0071239ba20a4e916c
Jan André Schlösser <jan.schloesser@it4need.de>
2018-11-01 19:21:12 +0100

    :bug: Employee class must initialize constructor with parameters

--- app/models/Employee.py
+++ app/models/Employee.py
@@ -5,7 +5,7 @@ from app.core.model import BaseModel
 
 class Employee(BaseModel):
     def __init__(self):
-        self.file_name = 'employee'
-        self.data_attributes = ['last_name', 'first_name', 'position']
-        BaseModel.__init__(self)
+        file_name = 'employee'
+        data_attributes = ['last_name', 'first_name', 'position']
+        BaseModel.__init__(self, file_name, data_attributes)
         print(self.data)
