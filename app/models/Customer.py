9562abe935f9bef87e77d4df2061aefe5d22f4f9
Jan André Schlösser <jan.schloesser@it4need.de>
2018-11-04 16:39:57 +0100

    :ok_hand: Change description to label

--- app/models/Customer.py
+++ app/models/Customer.py
@@ -6,5 +6,5 @@ from app.core.model import BaseModel
 class Customer(BaseModel):
     def __init__(self):
         file_name = 'customer'
-        data_attributes = ["customer_id", "description", "contact", "city"]
+        data_attributes = ["customer_id", "label", "contact", "city"]
         BaseModel.__init__(self, file_name, data_attributes)
